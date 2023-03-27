9649e363bc681cc65217dd401be68393ef962d96
jmosier <john.mosier@sbcglobal.net>
2023-03-26 21:06:03 -0400

    Update sketch_mar26a.ino
    added comment

--- sketch_mar26a/sketch_mar26a.ino
+++ sketch_mar26a/sketch_mar26a.ino
@@ -1,5 +1,6 @@
 //Code for the QRE1113 Analog board
 //Outputs via the serial terminal - Lower numbers mean more reflected
+//my middle name is patrick
 int QRE1113_Pin = 0; //connected to analog 0
 
 void setup(){
